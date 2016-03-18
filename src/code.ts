16894af9dc09f12cd3b98beb21951c6fd1bd32e0
ABA Games <cs8k-cyu@asahi-net.or.jp>
2016-03-19 00:11:42 +0900

    update the sample code

--- src/code.ts
+++ src/code.ts
@@ -1,7 +1,7 @@
 import {protoSpawn as ps, p5js as p, mech as m} from './protospawn';
 
 function setPsCode() {
-    ps.main = function* () {
+    ps.main = function*() {
         this.set({mechs: [
             new m.Condition.LimitActorCount().set({name: ['exploderPly', 'exploderEnm']})
         ]});
@@ -9,28 +9,41 @@ function setPsCode() {
         ps.enm();
     }
     ps.ply = function*(x = 50) {
+        let barrier = ps.barrier(this.pos);
         this.set({pos: {x, y: 90}, baseSpeed: 2, size: 7, mechs: [
             new m.AvatarMove.Direction().set({isVertical: false}),
             new m.EndOfScreen.Clamp(),
             new m.Collision.Test().set({name: ['bulletEnm', 'explosion'], do: (s, o) => {
                 if (s.remove()) {
+                    barrier.remove();
                     ps.delaySpawn(30, ps.ply, [this.pos.x]);
                 }
             }}),
             new m.AvatarInput.KeyDown().set({key: p.Key.button2, interval: 5, do: () => {
-                ps.bullet({pos: this.pos, angle: -p.HALF_PI, name: 'bulletPly'});
+                if (!p.isKeyDown(p.Key.button1)) {
+                    ps.bullet({pos: this.pos, angle: -p.HALF_PI, rename: 'bulletPly'});
+                }
             }}),
             new m.AvatarInput.KeyPressed().set({key: p.Key.button3, do: () => {
-                ps.exploder({pos: this.pos, vel: {y: -5}, name: 'exploderPly'});
+                if (!p.isKeyDown(p.Key.button1)) {
+                    ps.exploder({pos: this.pos, vel: {y: -5}, rename: 'exploderPly'});
+                }
             }}),
             new m.Event.Frame().set({do: () => {
-                if (p.isKeysDown(p.Key.button1)) {
+                if (p.isKeyDown(p.Key.button1)) {
                     this.mechs[0].speed = this.baseSpeed * 0.5;
                 } else {
                     this.mechs[0].speed = this.baseSpeed;
                 }
+                barrier.isVisible = p.isKeyDown(p.Key.button1) || 
+                (this.vel.x === 0 && !p.isKeyDown(p.Key.button2) && !p.isKeyDown(p.Key.button3));
             }})
         ]});
+    }
+    ps.barrier = function*(pos) {
+        this.size = 18;
+        this.fill = 'rgba(0, 0, 0, 0)';
+        this.pos = pos;
     }
     ps.enm = function*() {
         this.set({
@@ -46,8 +59,8 @@ function setPsCode() {
                 }
             }}),
             new m.Event.Random().set({probability: 0.05, do: (a) => {
-                ps.bullet({pos: this.pos, angle: p.HALF_PI, name: 'bulletEnm'});
-                ps.exploder({pos: this.pos, vel: {y: 5}, name: 'exploderEnm'});
+                ps.bullet({pos: this.pos, angle: p.HALF_PI, rename: 'bulletEnm'});
+                ps.exploder({pos: this.pos, vel: {y: 5}, rename: 'exploderEnm'});
             }}),
         ]});
     }
@@ -55,7 +68,7 @@ function setPsCode() {
         this.set(prop);
         this.speed = this.size = 3;
         this.mechs = [
-            new m.Collision.TestAndRemove().set({name: 'explosion'})
+            new m.Collision.TestAndRemove().set({name: ['explosion', 'barrier']})
         ];
     }
     ps.exploder = function*(prop) {
